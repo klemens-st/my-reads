6eadbf53758cfe8daf9d4f805203f01c12e0b6f3
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-07-22 08:34:49 +0200

    style: Add missing semicolons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
@@ -199,8 +199,8 @@ class BooksApp extends React.Component {
           </div>
         )}
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
